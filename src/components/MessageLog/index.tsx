c4157c39beba50903099f9ea3d08fd65931b8a4e
Steve Tu <stevetu11@yahoo.com>
2025-08-15 14:51:25 -0700

    Create initial starting components for the Home screen

---
+++ src/components/MessageLog/index.tsx
@@ -0,0 +1,38 @@
+import DeviceInfo from "react-native-device-info"
+import { useContext } from "react"
+import { MessageLogContext } from "../../context/MessageLogContext"
+import { ScrollView, StyleSheet, Text, View } from "react-native"
+
+const styles = StyleSheet.create({
+    logInnerContainer: {
+        height: "90%",
+        width: "100%",
+        backgroundColor: "#2f2f2f",
+        borderStyle: "solid",
+        borderRadius: 25,
+        marginBottom: 10,
+        elevation: 10,
+    },
+    logText: {
+        color: "white",
+        margin: 20,
+        fontSize: 8,
+    },
+})
+
+const MessageLog = () => {
+    const mlc = useContext(MessageLogContext)
+
+    const introMessage = `****************************************\nWelcome to ${DeviceInfo.getApplicationName()} v${DeviceInfo.getVersion()}\n****************************************\nInstructions\n----------------\nNote: The START button is disabled until the following steps are followed through.\n
+    1. TODO\n****************************************\n`
+
+    return (
+        <View style={styles.logInnerContainer}>
+            <ScrollView>
+                <Text style={styles.logText}>{introMessage + mlc.messageLog.join("\r")}</Text>
+            </ScrollView>
+        </View>
+    )
+}
+
+export default MessageLog
