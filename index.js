773a0431a892673dd7014f44170b4f7a1f4d3de4
Steve Tu <stevetu11@yahoo.com>
2025-08-27 23:44:40 -0700

    Set up Nativewind
    - In preparation for React Native Reusables.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry } from "react-native"
 import App from "./src/App"
+import "./global.css"
 
 // For Expo, register as "main".
 AppRegistry.registerComponent("main", () => App)
