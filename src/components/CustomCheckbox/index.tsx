67431227c7812801cd53df9efffbda55cac4caae
steve1316 <tustev00@gmail.com>
2025-09-27 20:01:22 -0700

    Fix bug where CustomCheckbox description would overflow past the screen
    - Also makes the label text bold.

--- src/components/CustomCheckbox/index.tsx
+++ src/components/CustomCheckbox/index.tsx
@@ -19,14 +19,21 @@ const CustomCheckbox: React.FC<CustomCheckboxProps> = ({ id = undefined, checked
     const { colors } = useTheme()
 
     return (
-        <View className={`flex flex-row items-start gap-3 ${className}`} style={style}>
+        <View className={`flex-row items-start gap-3 ${className}`} style={style}>
             <Checkbox id={id} checked={checked} onCheckedChange={onCheckedChange} className="dark:border-gray-400 dark:bg-gray-800" />
-            <View className="flex-1 gap-2">
-                <Label style={{ color: colors.foreground }} className="text-foreground" onPress={() => onCheckedChange(!checked)}>
+
+            {/* flexShrink is used to make sure the description wraps properly and not overflow past the right side of the screen. */}
+            <View style={{ flexShrink: 1 }}>
+                <Label style={{ color: colors.foreground, fontWeight: "bold" }} onPress={() => onCheckedChange(!checked)}>
                     {label}
                 </Label>
                 {description && (
-                    <Text style={{ color: colors.mutedForeground }} className="text-muted-foreground text-sm">
+                    <Text
+                        className="text-sm mt-1"
+                        style={{
+                            color: colors.mutedForeground,
+                        }}
+                    >
                         {description}
                     </Text>
                 )}
